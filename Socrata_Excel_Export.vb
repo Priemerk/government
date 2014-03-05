949f0485c7d491fda40037f188883aae52f48e3d
Kristopher Priemer <priemerk@gmail.com>
2014-03-05 12:47:02 -0500

    Update Socrata_Excel_Export.vb

--- Socrata_Excel_Export.vb
+++ Socrata_Excel_Export.vb
@@ -3,13 +3,13 @@ Private Sub Workbook_BeforeSave(ByVal SaveAsUI As Boolean, Cancel As Boolean)
 Application.DisplayAlerts = False
 Application.ScreenUpdating = False
 
-//.csv file that gets uploaded with DataSync
+'.csv file that gets uploaded with DataSync
 Workbooks.Open Filename:="F:\data_for_web.csv"
 
-//Original Excel file that has the data
+'Original Excel file that has the data
 Workbooks("Data_Master.xlsm").Activate
 
-//Activate proper tab
+'Activate proper tab
 Worksheets("Property Data").Activate
 
 Range("A3:BP1200").Select
